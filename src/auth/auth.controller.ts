98951c823fdbc765ae86712b7800f125e891f0a8
tomasrpita <tomasrpita@gmail.com>
2024-06-09 09:40:54 +0200

    feat: integrate Auth-MS with client gateway using NATS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,11 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /*
+    foo.* matches foo.bar, foo.baz, and so on, but not foo.bar. baz
+    foo.*. bar matches foo.baz.bar, foo.qux.bar, and so on, but not foo.bar or foo.bar.baz
+    foo. › matches foo.bar, foo.bar.baz, and so on
+• */
   @MessagePattern('auth.register.user')
   registerUser() {
     return 'register user';
